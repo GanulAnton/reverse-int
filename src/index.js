53f785f398414e3a60eec4606b28e7ca17655f5b
GanulAnton <47523214+GanulAnton@users.noreply.github.com>
2020-02-26 21:11:12 +0300

    init: update function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    var mN = Math.abs(n);
+    var sN = mN.toString();
+    var rN = sN.split('').reverse().join('');
+    var a = Number(rN);
+    return (a);
 }
